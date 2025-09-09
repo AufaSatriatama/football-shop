f35ce3a3bba034040ddf42f1981d8ccdae1a1922
AufaSatriatama <aufa4satriatama@gmail.com>
2025-09-10 00:51:11 +0700

    Add dashboard page

--- main/views.py
+++ main/views.py
@@ -11,3 +11,14 @@ def show_main(request):
     }
 
     return render(request, "main.html", context)
+
+def show_dashboard(request):
+
+    context = {
+        'appName': 'Jual Jersey',
+        'npm' : '2406426321',
+        'name': 'Aufa Daffa Satriatama',
+        'class': 'PBP B'
+    }
+
+    return render(request, "dashboard.html", context)
